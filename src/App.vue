d4a4cea1a889259027ab7482abfe71a509c3e9c0
Felipe Cardozo <dafelcardozo@gmail.com>
2024-04-30 21:10:37 -0500

    Updated the navigation bar and added a navigation drawer.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
+    <v-navigation-drawer v-model="drawer" temporary app>
+      <v-list  nav>
+          <v-list-item link title="Home" :to="{name: 'Home'}" value="home">Home</v-list-item>
+          <v-list-item link title="Data" :to="{name: 'Data'}" value="data">Data</v-list-item>
+          <v-list-item link title="About" :to="{name: 'About'}" value="about">About</v-list-item>
+      </v-list>
+      </v-navigation-drawer>
+    <v-app-bar app color="primary" dark clipped-left >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <div class="d-flex align-center">
+          <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
@@ -19,26 +27,53 @@
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
         />
-      </div>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-app-bar-title>Nav buttons:</v-app-bar-title>
-      <v-btn :to="{name: 'Home'}" >Home</v-btn>
-      <v-btn :to="{name: 'Data'}" >Data</v-btn>
-      <v-btn :to="{name: 'About'}">About</v-btn>
-    </v-app-bar>
 
+        </div>
+        <v-spacer></v-spacer>
+          <v-btn :to="{name: 'Home'}" variant="text" color="primary">Home</v-btn>
+          <v-btn :to="{name: 'Data'}" variant="text" color="primary">Data</v-btn>
+          <v-btn :to="{name: 'About'}" variant="text" color="primary">About</v-btn>
+      </v-app-bar>
     <v-main>
+      <v-container>
       <router-view />
+      </v-container>
     </v-main>
-  </v-app>
+ </v-app>
+ 
 </template>
 
 <script>
 export default {
   name: "App",
-
   data: () => ({
-    //
-  }),
+      drawer: false,
+      group: null,
+      items: [
+        {
+          title: 'Foo',
+          value: 'foo',
+        },
+        {
+          title: 'Bar',
+          value: 'bar',
+        },
+        {
+          title: 'Fizz',
+          value: 'fizz',
+        },
+        {
+          title: 'Buzz',
+          value: 'buzz',
+        },
+      ],
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  
 };
 </script>
