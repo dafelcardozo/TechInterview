a441312f5adfbf91bf80354341345df7b1cc6028
Felipe Cardozo <dafelcardozo@gmail.com>
2024-04-30 19:22:09 -0500

    Location column.

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -11,15 +11,17 @@
                     <v-img :src="item.avatar" :alt="item.name" height="60px" width="50px"></v-img>
                   </div>
                 </template>
+                <template v-slot:item.address="{ item }">
+                  <div class="p-2">
+                    {{ item.address.city }}, {{ item.address.country }}
+                  </div>
+                </template>
             </v-data-table>
 
             <!-- v-card-subtitle><p>Uh oh. There is nothing there. Using the data at <a href="https://random-data-api.com/">https://random-data-api.com/</a>, populate the table
       below with headers and make it searchable. Also, expand the table to full width of the container. Let Bob know when complete.</p></v-card-subtitle -->
           
     </v-card>
-    </v-row>
-    <v-row>
-      <p>adasd</p>
     </v-row>
   </v-container>
 </template>
@@ -33,9 +35,10 @@ export default {
     return {
       persons: [],
       headers: [
+        {text: "Avatar", sortable:false, value:"avatar"},
         { text: "Name", align: "start", sortable: true, value: "first_name" },
-        { text: "Last name",  sortable: true, value: "last_name" },
-        {text: "Avatar", sortable:false, value:"avatar"}
+        { text: "Last name",  sortable: true, value: "last_name" },      
+        {text:"Location", sortable:false, value:"address"}
       ],
     }
   },
@@ -43,6 +46,7 @@ export default {
   },
   async mounted () {
     const {data}  = await axios.get('https://random-data-api.com/api/v2/users?size=10');
+    console.info(data);
     this.persons = data;    
   }
 };
